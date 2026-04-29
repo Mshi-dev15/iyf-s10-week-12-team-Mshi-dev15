78948524440566921def5ae7a5040242461efafa
vivian <macuavivianne@gmail.com>
2026-04-30 01:50:45 +0300

    modified post text display in Posts.jsx

--- frontend/src/pages/Posts.jsx
+++ frontend/src/pages/Posts.jsx
@@ -1,7 +1,3 @@
-export default function Posts() { return <div className="p-6"><h1 className="text-2xl font-bold">Opportunities</h1><p className="mt-2 text-gray-600">List view coming soon.</p></div> }
-import useFetch from "../hooks/useFetch";
-import { getPosts } from "../services/postsAPI";
-
 export default function Posts() {
   const { data, loading, error } = useFetch(getPosts);
 
@@ -10,7 +6,7 @@ export default function Posts() {
 
   return (
     <div>
-      {data.posts.map((post) => (
+      {data?.posts?.map((post) => (
         <div key={post.id}>{post.title}</div>
       ))}
     </div>
